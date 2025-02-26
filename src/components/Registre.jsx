7d273cca7ddbf847b9e4976466b3ed9051964c4d
Pep V <jvidal5@xarxa.fedac.cat>
2025-02-26 20:47:35 +0100

    registre fet, cal modificar

--- src/components/Registre.jsx
+++ src/components/Registre.jsx
@@ -1,14 +1,54 @@
 import '../App.css';
+import React, { useState } from "react";
+import { Link,useNavigate } from 'react-router-dom';
+import joposep from '../imagenes/joj.png';
 
-function Registre() {
+export default function AuthForm() {
+    const navigate = useNavigate();
 
-    return (
-    <div className="contenidoregistre">
-        <div className='cuadro'>
+    const handleRedirect = () => {
+        navigate("/"); // Redirige a la página principal
+    };
+    
+    const Registre = () => {
+        navigate("/Registre")
+    };
 
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="form-box">
+        <Link to="/">
+            <img className="logo" src={joposep} alt="Logo" />
+            <button onClick={handleRedirect} className='botologo'></button>
+        </Link>
+        <div className="tab-menu">
+          <a href="#" className={!isLogin ? "active" : ""} onClick={() => setIsLogin(false)}>Registra’t</a>
+          <a href="#" className={isLogin ? "active" : ""} onClick={() => setIsLogin(true)}>Inicia sessió</a>
         </div>
+        <p className="description">
+          {isLogin ? "Ingresa les teves dades per a iniciar sessió i poder opinar." : "Ingresa les teves dades per a crear un compte i poder opinar."}
+        </p>
+        <div className="input-group">
+          <label>Correu electrònic</label>
+          <input type="email" className="input-field" />
+        </div>
+        {!isLogin && (
+          <div className="input-group">
+            <label>Nom d’usuari</label>
+            <input type="text" className="input-field" />
+            <p className="info-text">15 characters max, letters and numbers only</p>
+          </div>
+        )}
+        <div className="input-group">
+          <label>Contrasenya</label>
+          <input type="password" className="input-field" />
+          <p className="info-text">Must be at least 6 characters with 1 number and special character</p>
+        </div>
+        <button className="submit-btn">{isLogin ? "Inicia sessió" : "Registra’t"}</button>
+        {isLogin && <p className="forgot-password">Has oblidat la contrasenya?</p>}
+      </div>
     </div>
-    );
-};
-export default Registre;
-
+  );
+}
