f21d16d4037046cf3df5bd3f3e80b6250dc9560d
Pep V <jvidal5@xarxa.fedac.cat>
2025-02-26 18:28:12 +0100

    petita tipografia nomas

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -31,7 +31,7 @@ function Anime() {
                 </Link>
                 <Link to="/videojocs">Videojocs</Link>
                 <Link to="/anime">Anime</Link>
-                <Link to="/Pelis">Pel·lícules</Link>
+                <Link to="/pelis">Pel·lícules</Link>
                 <Link to="/series">Series</Link>
                 <Link to="/restaurants">Restaurants</Link>
                 <div className='fuentebusqueda'>
