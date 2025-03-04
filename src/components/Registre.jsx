66d49b1d59dd152c01b654f1a8737ff36b8e0375
Pep V <jvidal5@xarxa.fedac.cat>
2025-03-04 17:26:43 +0100

    millores en general

--- src/components/Registre.jsx
+++ src/components/Registre.jsx
@@ -16,7 +16,7 @@ export default function AuthForm() {
     <div className="container">
       <div className="form-box">
         <Link to="/">
-            <img className="logo" src={joposep} alt="Logo" />
+            <img className="logo_registre" src={joposep} alt="Logo" />
             <button onClick={handleRedirect} className='botologo'></button>
         </Link>
         <div className="tab-menu">
