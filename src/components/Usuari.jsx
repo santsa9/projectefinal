4e4106cd8c1c8f30c44c457f0abf63b533f9311c
Pep V <jvidal5@xarxa.fedac.cat>
2025-03-04 16:26:39 +0100

    primers cambis a millor

--- src/components/Usuari.jsx
+++ src/components/Usuari.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from 'react-router-dom';
 import '../Usuaricss.css';
 import joposep from "../imagenes/joj.png";
 import lupa from "../imagenes/lupa.png";
@@ -7,29 +7,22 @@ import usuari from "../imagenes/use.png";
 
 const categories = ["Videojocs", "Pel·lícules", "Sèries", "Anime", "Restaurants"];
 
-const ratings = {
-  "Pel·lícules": [
-    { title: "Títol peli 1", date: "01/01/2025", rating: "⭐⭐⭐⭐", comment: "Molt bona!" },
-    { title: "Títol peli 2", date: "15/02/2025", rating: "⭐⭐⭐", comment: "Podria ser millor." }
-  ],
-  "Videojocs": [],
-  "Sèries": [],
-  "Anime": [],
-  "Restaurants": []
-};
-
 const Usuari = () => {
   const [selectedCategory, setSelectedCategory] = useState("Pel·lícules");
   const navigate = useNavigate();
 
+  const handleRedirect = () => {
+    navigate("/"); // Redirige a la página principal
+};
   return (
     <div className="app">
       {/* Capçalera */}
       <header className="header">
         <nav className="nav">
-          <button onClick={() => navigate("/")} className="botologo">
-            <img className="logo" src={joposep} alt="Logo" />
-          </button>
+          <Link to="/">
+                <img className="logo" src={joposep} alt="Logo" />
+                <button onClick={handleRedirect} className='botologo'></button>
+          </Link>
           <a href="/Videojocs">Videojocs</a>
           <a href="/Anime">Anime</a>
           <a href="/Pelis">Pel·lícules</a>
@@ -70,21 +63,6 @@ const Usuari = () => {
               </button>
             ))}
           </nav>
-
-          <section className="ratings">
-            {ratings[selectedCategory].length > 0 ? (
-              ratings[selectedCategory].map((item, index) => (
-                <div key={index} className="rating-card">
-                  <h3>{item.title}</h3>
-                  <p><strong>Data:</strong> {item.date}</p>
-                  <p><strong>Valoració:</strong> {item.rating}</p>
-                  <p>{item.comment}</p>
-                </div>
-              ))
-            ) : (
-              <p>No hi ha valoracions en aquesta categoria.</p>
-            )}
-          </section>
         </main>
       </div>
     </div>
